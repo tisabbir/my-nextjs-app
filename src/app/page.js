1b26262fed4abba637e8e08f736710da4056e0b8
TI Sabbir <tisabbir0@gmail.com>
2024-06-19 16:52:43 +0600

    tailwind and daisy ui added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 import Counter from "@/components/Counter/Counter";
+import Link from "next/link";
 
 const HomePage = () => {
   return (
     <div>
       <h1>This Is Next home </h1>
       <Counter />
+
+      <Link href={"/about"}>
+        <button className="btn btn-primary">About</button>
+      </Link>
     </div>
   );
 };
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,9 +1,12 @@
+
 import React from 'react';
 
 const ProductsPage = () => {
     return (
         <div>
            <h1> This is Products Paged</h1>
+
+           
         </div>
     );
 };
